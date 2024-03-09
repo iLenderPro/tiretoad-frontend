929ba5523090b461619d7688af7337ac47a3e5b4
Nick Zakonov <nick.zakonov@gmail.com>
2024-03-09 19:31:58 +0400

    ci(frontend): added verification file for cert issuing

--- shared/ui/Auth/Auth.tsx
+++ shared/ui/Auth/Auth.tsx
@@ -4,20 +4,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useGetUserSessionQuery } from '@/entities/account/api/accountApi';
 import { PropsWithChildren, useEffect } from 'react';
 import { redirect, usePathname } from 'next/navigation';
+import { UserRole } from '@/entities/user/api/dto/UserRole';
 
 export default function Auth({ children }: PropsWithChildren) {
   const pathname = usePathname();
   const dispatch = useDispatch();
   const session = useSelector(selectUserSession);
   const { data } = useGetUserSessionQuery(undefined, { pollingInterval: 5 * 60 * 1000 });
-  console.log('session', session);
+
   useEffect(() => {
     data && dispatch(setUserSession({ user: data }));
   }, [data, dispatch]);
-
   useEffect(() => {
     if (!session?.authToken && pathname !== '/' && pathname !== '/login') {
       redirect('/login?redirect_url=' + pathname);
+    }
+    console.log(session?.authToken);
+    if (session?.authToken && session?.user?.role === UserRole.VENDOR && pathname === '/') {
+      redirect('/requests');
     }
   }, []);
 
