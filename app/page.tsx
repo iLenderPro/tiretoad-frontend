ae7a508bacf87f6cd2e424d2594f9c9471071b41
Nick Zakonov <nick.zakonov@gmail.com>
2024-07-11 14:47:32 +0400

    feat(frontend): added metadata  for index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import ServiceRequestWizard from '@/features/ui/ServiceRequestWizard/ServiceRequestWizard';
 import { Container } from '@mui/material';
+import type { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: `Mobile tire repair shops - TireToad`,
+  description: `Get your flat tire fixed in in 30 minutes or less. Connect with a mobile tire repair service right in your area instantly.`,
+};
 export default function Home() {
   return (
     <Container style={{ display: 'flex', flex: 1, paddingTop: '24px' }}>
